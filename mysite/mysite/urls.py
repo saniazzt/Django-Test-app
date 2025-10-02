2b0f360c8ed47d2042f61e812979db00efad3647
saniaezzati <saniaezzati1384@gmail.com>
2025-10-02 17:59:52 +0200

    update frontend with Bootstrap

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("tests/", include("tests.urls")),
+    path("", include("tests.urls")),
     path('admin/', admin.site.urls),
 ]
--- mysite/tests/views.py
+++ mysite/tests/views.py
@@ -1,65 +1,49 @@
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect
-from django.http import Http404
-
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
-
-from .models import Choice, Test,Question
+from .models import Choice, Test
 
 
 def index(request):
+    """Show list of latest tests"""
     latest_test_list = Test.objects.order_by("-pub_date")[:5]
     context = {"latest_test_list": latest_test_list}
-    return render(request, "tests/index.html", context)
+    return render(request, "index.html", context)
 
 
 def detail(request, test_id):
-    try:
-        test = Test.objects.get(pk=test_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "tests/detail.html", {"test": test})
-#    return HttpResponse("You're looking at test %s." % test_id)
-
-
-def results(request, test_id):
+    """Show a single test with its questions and choices"""
     test = get_object_or_404(Test, pk=test_id)
-    return render(request, "tests/results.html", {"test": test})
-
+    return render(request, "detail.html", {"test": test})
 
 
 def answer(request, test_id):
+    """Process submitted answers and redirect to results"""
     test = get_object_or_404(Test, pk=test_id)
-    selected_choice = []
+    selected_choices = []
 
-    # question1 = test.question_set.get(pk=1)
-    # question2 = test.question_set.get(pk=2)
     for question in test.question_set.all():
-        try:
-            selected_choice1 = question.choice_set.get(pk=request.POST["choice_{}".format(question.pk)])
-            # selected_choice2 = question2.choice_set.get(pk=request.POST["choice_2"])
-            selected_choice.append( selected_choice1 )
-            
-        except (KeyError, Choice.DoesNotExist):
-            pass
-            # # Redisplay the question voting form.
-            # return render(
-            #     request,
-            #     "tests/detail.html",
-            #     {
-            #         "test": test,
-            #         "error_message": "You didn't select a choice.",
-            #     },
-            # )
+        choice_id = request.POST.get(f"choice_{question.pk}")
+        if choice_id:
+            try:
+                choice = question.choice_set.get(pk=choice_id)
+                selected_choices.append(choice)
+            except Choice.DoesNotExist:
+                pass
 
-        # selected_choice1.save()
-        # selected_choice2.save()
-        # selected_choice [0].save()
-    test.total_score = 0
-    test.save()    
-    for choice in selected_choice:
-        test.total_score += choice.score
-        test.save()
+    # Calculate score
+    total_score = sum(choice.score for choice in selected_choices)
+
+    # Store score in session so results view can access it
+    request.session["last_score"] = total_score
+
     return HttpResponseRedirect(reverse("tests:results", args=(test.id,)))
-#    return HttpResponse("You're answering test %s" % test_id , "question %s." % question_id)
+
+
+def results(request, test_id):
+    """Show the result page with the computed score"""
+    test = get_object_or_404(Test, pk=test_id)
+    score = request.session.get("last_score", None)
+
+    return render(request, "results.html", {"test": test, "score": score})
